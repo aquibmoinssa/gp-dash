551e7dd53cb23c38c613309b044adcfcda9f6219
aquibmoinssa <106266370+aquibmoinssa@users.noreply.github.com>
2024-06-27 05:48:42 +0000

    plot update

--- dashboard.py
+++ dashboard.py
@@ -11,15 +11,15 @@ with open('style.css') as f:
     
 st.sidebar.header('GP Dashboard')
 
-st.sidebar.subheader('Heat map parameters')
+#st.sidebar.subheader('Heat map parameters')
 #time_hist_color = st.sidebar.selectbox('Color by', ('temp_min', 'temp_max')) 
 
-st.sidebar.subheader('Donut chart parameters')
-donut_theta = st.sidebar.selectbox('Select data', ('q2', 'q3'))
+#st.sidebar.subheader('Donut chart parameters')
+#donut_theta = st.sidebar.selectbox('Select data', ('q2', 'q3'))
 
 st.sidebar.subheader('Line chart parameters')
 # Sidebar slider to specify plot height
-plot_height = st.sidebar.slider('Specify plot height', 300, 600, 400)
+#plot_height = st.sidebar.slider('Specify plot height', 300, 600, 400)
 #plot_width = st.sidebar.slider('Specify plot width', 600, 1000, 700)
 
 
@@ -51,8 +51,6 @@ epochs = pd.to_datetime(epochs)
 # Create a DataFrame for easier manipulation
 df = pd.DataFrame({'Epoch': epochs, 'Period': periods, 'Eccentricity': eccentricity, 'Mean Anomaly': mean_anomaly})
 
-seattle_weather = pd.read_csv('https://raw.githubusercontent.com/tvst/plost/master/data/seattle-weather.csv', parse_dates=['date'])
-stocks = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/master/stocks_toy.csv')
 
 # Create two columns
 c1, c2 = st.columns((5, 5))
@@ -79,21 +77,17 @@ with c2:
     )
     st.altair_chart(line_chart_eccentricity, use_container_width=True)
 
-# Row C
-# Set up the Streamlit app
-
 # Create an Altair chart with customizable height and width
 st.markdown('### Line Chart of Period Over Time')
 chart = alt.Chart(df).mark_line(color='green').encode(
     x=alt.X('Epoch:T', title='Epoch'),
     y=alt.Y('Period:Q', title='Orbital Period (minutes)', scale=alt.Scale(domain=[94.8, 95.2]))
 ).properties(
-    height=plot_height,
-    width=700  # Adjust width as well
+    height=600,
+    width=400  # Adjust width as well
 )
 
 # Display the chart in the Streamlit app
 st.altair_chart(chart, use_container_width=True)
 
-#st.markdown('### HST Orbit')
-#st.line_chart(df.set_index('Epoch')['Period'])
+
