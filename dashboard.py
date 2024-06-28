c995280790d723244d657bd8b4e204fcd7b86f53
aquibmoinssa <106266370+aquibmoinssa@users.noreply.github.com>
2024-06-28 02:13:06 +0000

    formatting edits

--- dashboard.py
+++ dashboard.py
@@ -30,7 +30,7 @@ col1, col2, col3 = st.columns(3)
 col1.metric("Temperature", "70 °F", "1.2 °F")
 col2.metric("Wind", "9 mph", "-8%")
 col3.metric("Humidity", "86%", "4%")
-<br><br>
+st.markdown("""<br><br>""", unsafe_allow_html = True)
 
 # Row B
 
